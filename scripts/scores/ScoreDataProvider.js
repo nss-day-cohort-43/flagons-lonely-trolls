caa3187b3349e97347929de3f2746e111a492f6e
j0verton <overtonje@gmail.com>
2020-09-04 12:50:10 -0400

    added player and score objects

---
+++ scripts/scores/ScoreDataProvider.js
@@ -0,0 +1,38 @@
+const scores = [
+    {
+        id:1,
+        date: 12982304981263058174,
+        teamId: 3,
+        score: 37
+    },
+    {
+        id:2,
+        date: 12982304981263058174,
+        teamId: 2,
+        score: 54,
+    },
+    {
+        id: 3,
+        date: 12982304981263058185,
+        teamId: 3,
+        score: 37
+    },
+    {
+        id:4,
+        date: 12982304981263058174,
+        teamId: 3,
+        score: 37
+    },   
+    {
+        id:5,
+        date: 12982304981263058174,
+        teamId: 3,
+        score: 37
+    },
+    {
+        id:6,
+        date: 12982304981263058174,
+        teamId: 2,
+        score: 54
+    },
+]
