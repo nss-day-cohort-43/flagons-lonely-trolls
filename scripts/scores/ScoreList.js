47f17d806f8668e94935936d5e9b5bdedeed68a2
Ricky Blake <rab0196@yahoo.com>
2020-09-04 14:05:49 -0500

    teams are being added to DOM from database

--- scripts/scores/ScoreList.js
+++ scripts/scores/ScoreList.js
@@ -3,14 +3,12 @@ import { Score } from "./Score.js"
 
 export const ScoreList = () => {
 
-    const contentElement = document.querySelector("body")
+    const contentElement = document.querySelector("#scoreContainer")
     const scoreArray = useScores()
 
     // Add a section, and all of the fish to the DOM
     contentElement.innerHTML += `
-        <section id="scoreContainer">
-            <h2>Scores</h2>
-            ${scoreArray.map(score => Score(score)).join("")}
-        </section>
+        <h2>Scores</h2>
+        ${scoreArray.map(score => Score(score)).join("")}
     `
 }
