f5c0a398796963490d57ddb462ff1239da441370
j0verton <overtonje@gmail.com>
2020-09-04 15:13:36 -0400

    playerform in process needs event listener

---
+++ scripts/players/PlayerForm.js
@@ -0,0 +1,45 @@
+import { getTeams, useTeams } from "../teams/TeamDataProvider.js"
+
+
+export const renderPlayerForm = (teamList) => {
+    const form = `
+        <form id="newPlayerForm">
+            <fieldset class="newPlayerField">
+                <input type="text" name="firstName" id="firstName" maxlength="15" placeholder="First Name">
+            </fieldset> 
+            <fieldset id="newPlayerField">
+                <input type="text" name="lastName" id="lastName" maxlength="15" placeholder="Last Name">
+            </fieldset> 
+            <fieldset>
+            <label for="openTeams">Please select a team...</label>
+            <select name="team" id="selectOpenTeam">
+            ${
+                teamList.map( teamObj => {
+                    if (teamObj.teamSize < 3) {
+                        const openTeam = teamObj.name;
+                        return`<option value=${openTeam}>${openTeam}</option>`
+                    }
+                })               
+            }
+            </select>
+        </fieldset>
+            <button type="button" id="addPlayerBtn">Add Player to Team</button>
+        </form>
+        `
+
+        document.getElementById("leftAside").innerHTML += form
+}
+
+
+
+export const PlayerFormComponent = () =>{
+    getTeams()
+    .then(() => {
+        renderPlayerForm(useTeams());
+    })
+
+    eventHub.addEventListener
+} 
+
+
+//teamStateChanged event listener
