5a33406b04173d57b6808a943ecfcb426c352995
Ricky Blake <rab0196@yahoo.com>
2020-09-04 11:47:26 -0500

    fixed dates

--- scripts/teams/TeamDataProvider.js
+++ scripts/teams/TeamDataProvider.js
@@ -2,19 +2,19 @@ const teams = [
     {
         id: 1,
         name: "Lonely Trolls",
-        dateCreated: 9/1/2020,
+        dateCreated: "9/1/2020",
         totalScore: 98
     },
     {
         id: 2,
         name: "Green Dragons",
-        dateCreated: 8/21/2020,
+        dateCreated: "8/21/2020",
         totalScore: 65
     },
     {
         id: 3,
         name: "Shining Shields",
-        dateCreated: 9/3/2020,
+        dateCreated: "9/3/2020",
         totalScore: 73
     }
 
