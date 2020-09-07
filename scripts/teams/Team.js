5c45f54b68fb244d1e079c9e0379299e3319a7ac
j0verton <overtonje@gmail.com>
2020-09-07 15:30:33 -0400

    adding table for leader board

--- scripts/teams/Team.js
+++ scripts/teams/Team.js
@@ -1,13 +1,26 @@
   
 export const Team = (teamObj) => {
     return `
-    <div id="team-${teamObj.id}">
-        <ul>
-            <li class="teamProps">Team: ${teamObj.name}</li>
-            <li class="teamProps">Date Formed: ${teamObj.dateCreated}</li>
-            <li class="teamProps">Total Score: ${teamObj.totalScore}</li>
-            <li class="teamProps">Team Size: ${teamObj.teamSize}</li>
-        </ul>
-    </div>
-    `
-}
+        <table id="team-${teamObj.id}">
+            <tr>
+            <th>Name</th>
+            <th>Player</th>
+            <th>score</th>
+            </tr>
+            <tr>
+            <td>${teamObj.name}</td>
+            <td>${teamObj.teamSize}</td>
+            <td>${teamObj.totalScore}</td>
+            </tr>
+        </table>
+      
+`
+
+//             <li class="teamProps">Team: ${teamObj.name}</li>
+//             <li class="teamProps">Date Formed: ${teamObj.dateCreated}</li>
+//             <li class="teamProps">Total Score: ${teamObj.totalScore}</li>
+//             <li class="teamProps">Team Size: ${teamObj.teamSize}</li>
+//         </ul>
+//     </div>
+//     
+// }
--- scripts/teams/TeamList.js
+++ scripts/teams/TeamList.js
@@ -19,3 +19,4 @@ const addTeamsToDOM = arrayOfTeams => {
             ${HTMLArray.join("")}
     `
 }
+
