b343f9370a4caf477d478abb0e72b87f394924ff
j0verton <overtonje@gmail.com>
2020-09-08 13:01:09 -0400

    basic css layout achieved!

--- scripts/scores/ScoreDataProvider.js
+++ scripts/scores/ScoreDataProvider.js
@@ -1,40 +1,5 @@
 const scores = [
-    {
-        id:1,
-        date: 12982304981263058174,
-        teamId: 1,
-        score: 37
-    },
-    {
-        id:2,
-        date: 12982304981263058174,
-        teamId: 2,
-        score: 54,
-    },
-    {
-        id: 3,
-        date: 12982304981263058174,
-        teamId: 3,
-        score: 7
-    },
-    {
-        id:4,
-        date: 12982304981263058191,
-        teamId: 1,
-        score: 18
-    },   
-    {
-        id:5,
-        date: 12982304981263058191,
-        teamId: 2,
-        score: 37
-    },
-    {
-        id:6,
-        date: 12982304981263058191,
-        teamId: 3,
-        score: 54
-    },
+
 ]
 
 export const useScores = () => {
