618bcd26884bd514cdb62f81ecdbd88060a98327
j0verton <overtonje@gmail.com>
2020-09-17 15:33:36 -0500

    made functioing in game scoreboard

--- scripts/games/GameForm.js
+++ scripts/games/GameForm.js
@@ -2,6 +2,8 @@ import { renderTeamDropdowns } from "../teams/TeamSelect.js"
 import { useTeams } from "../teams/TeamDataProvider.js"
 import { RoundScoreForm } from "./RoundScoreForm.js"
 
+import {scoreBoard} from "./ScoreBoard.js"
+
 const eventHub = document.querySelector("body")
 let playingTeams = [];
 
@@ -20,12 +22,17 @@ eventHub.addEventListener("dropdownsSelected", event => {
     const teamThree = allTeams.find(team => team.id === event.detail.team3)
 
     playingTeams.push(teamOne, teamTwo, teamThree)
-
+    
+    playingTeams.map(team => team.totalScore=0)
+    
     const formHTML = RoundScoreForm(playingTeams)
     contentTarget.innerHTML = `
         <h4 class="roundHeader" id="1">Round 1</h4>
         ${formHTML}
     `
+
+    document.querySelector("#scoreContainer").innerHTML = scoreBoard(playingTeams);
+
 })
 
 eventHub.addEventListener("roundScored", event => {
@@ -38,9 +45,14 @@ eventHub.addEventListener("roundScored", event => {
             <h4 class="roundHeader" id="${event.detail.round}">Round ${event.detail.round}</h4>
             ${formHTML}
         `
+        playingTeams[0].totalScore += event.detail.teamOneScore
+        playingTeams[1].totalScore += event.detail.teamTwoScore
+        playingTeams[2].totalScore += event.detail.teamThreeScore
+        document.querySelector("#scoreContainer").innerHTML = scoreBoard(playingTeams);
     } else {
         contentTarget.innerHTML = `
             <p>You're done</p>
         `
     }
+
 })
