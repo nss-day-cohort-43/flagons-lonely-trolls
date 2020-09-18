0b6742b2575b80d958d1338aa0352574606915cd
j0verton <overtonje@gmail.com>
2020-09-17 20:53:49 -0500

    just a start

---
+++ scripts/games/Outcomes.js
@@ -0,0 +1,25 @@
+if (detail.round === 1){}else if (detail.round === 2){}else if (detail.round === 3)
+
+switch (detail) {
+    
+    case value:
+        
+        break;
+
+        case value:detail.round === 1
+        
+        break;
+
+    default: 
+        console.log(`great game i guess...`)
+        break;
+}
+
+
+/*         detail: {
+    round: roundNum,
+    teamOneScore: teamScores[0],
+    teamTwoScore: teamScores[1],
+    teamThreeScore: teamScores[2],
+}
+*/
