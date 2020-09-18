f5e0c1c3f720f946c302e4dd6c9c986900b8f78c
j0verton <overtonje@gmail.com>
2020-09-18 10:52:37 -0500

    adding a save function

--- scripts/scores/ScoreDataProvider.js
+++ scripts/scores/ScoreDataProvider.js
@@ -1,7 +1,19 @@
+
 const scores = [
 
 ]
 
 export const useScores = () => {
     return scores.slice()
+}
+
+
+const saveScores = (scoreArray) => {
+    scoreArray.map(scoreObj =>
+        delete scoreObj.teamName
+    )
+
+    fetch
+
+
 }
