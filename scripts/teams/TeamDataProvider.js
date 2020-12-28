16933aca3bb5ca53f9ac3b238aabb9d3e61e3315
j0verton <overtonje@gmail.com>
2020-12-28 11:30:52 -0600

    added deployment

--- scripts/teams/TeamDataProvider.js
+++ scripts/teams/TeamDataProvider.js
@@ -11,41 +11,41 @@ const dispatchStateChangeEvent = () => {
 export const useTeams = () => teams.slice()
 
 export const getTeams = () => {
-    return fetch('http://localhost:8088/teams')
+    return fetch('/teams')
         .then(response => response.json())
         .then(parsedTeams => teams = parsedTeams)
 }
 
 export const saveTeam = team => {
-    return fetch('http://localhost:8088/teams', {
+    return fetch('/teams', {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(team)
     })
-    .then(dispatchStateChangeEvent)
+        .then(dispatchStateChangeEvent)
 }
 
 export const updateTeam = (teamObj) => {
-    return fetch(`http://localhost:8088/teams/${teamObj.id}`, {
+    return fetch(`/teams/${teamObj.id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(teamObj)
     })
-    .then(dispatchStateChangeEvent)
+        .then(dispatchStateChangeEvent)
 }
 
 export const updateTeamScores = (teamArray) => {
     teamArray.forEach(teamObj => {
-    return fetch(`http://localhost:8088/teams/${teamObj.id}`,{
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(teamObj)
+        return fetch(`/teams/${teamObj.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(teamObj)
+        })
     })
-})
 }
